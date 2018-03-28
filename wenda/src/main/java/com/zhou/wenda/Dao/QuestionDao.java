e5ca883adc3dd73f969fcdc92711efbc114ad134
周武栋 <wzhou1_intern@xiaohongshu.com>
2018-03-28 10:30:49 +0800

    questionsDao

--- wenda/src/main/java/com/zhou/wenda/Dao/QuestionDao.java
+++ wenda/src/main/java/com/zhou/wenda/Dao/QuestionDao.java
@@ -1,6 +1,43 @@
 package com.zhou.wenda.Dao;
 
+import com.zhou.wenda.domain.Question;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+
 public interface QuestionDao {
 
+    /**
+     * 添加
+     *
+     * @param question
+     * @return
+     */
+    @Insert(value = "insert into question(title, content, userId, createDate,commentCount) values (#{title}, #{content}, #{userId}, #{createdDate}, #{commentCount})")
+    boolean insertQuestion(Question question);
 
+    /**
+     * 删除
+     */
+    @Delete("delete from question where id = #{id}")
+    void deleted(int id);
+
+    /**
+     * 修改
+     */
+    @Update(value = "update question set comment_count = #{count} where id = #{id}")
+    boolean updateCommentCount(int id, int count);
+
+   /**
+     * find
+     */
+    @Select("select * from user where id = #{id}")
+    Question findById(int id);
+
+
+    @Select("select * from question where id = #{userId} limit #{offset}, #{limit}")
+    List<Question> selectLatestQuestions(int userId,int offset, int limit);
 }
