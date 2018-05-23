d7b9ac9ea03fca0074b7d1dcbf674d9693e9036e
周武栋 <wzhou1_intern@xiaohongshu.com>
2018-05-23 21:14:37 +0800

    fix log

--- wenda/src/main/java/com/zhou/wenda/Controller/FeedController.java
+++ wenda/src/main/java/com/zhou/wenda/Controller/FeedController.java
@@ -7,8 +7,6 @@ import com.zhou.wenda.service.FeedService;
 import com.zhou.wenda.service.FollowService;
 import com.zhou.wenda.service.RedJedis;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -22,7 +20,6 @@ import java.util.List;
 @Slf4j
 @Controller
 public class FeedController {
-    private static final Logger logger = LoggerFactory.getLogger(FeedController.class);
 
     @Resource
     private HostHolder hostHolder;
